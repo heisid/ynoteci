3011c461e6f36aa44fad02b62a3a5de82b8f99fe
heisid <weird_alien@protonmail.com>
2018-07-19 17:34:19 +0700

    search controller complete

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -1,3 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Search extends CI_Controller {
+    public function index() {
+        $this->load->model('Post_model');
+        $post = $this->Post_model;
+
+        $search_string = $this->input->post('search');
+        $list_posts = $post->search_post($search_string);
+
+        $this->load->view('header_view', array('title_post' => 'Search - Y-Note'));
+        $this->load->view('navbar_view');
+        $this->load->view('search_view', array('list_posts' => $list_posts));
+        $this->load->view('tail_view');
+    }
+}
