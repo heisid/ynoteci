af36d488ff900fba00e98e13341406e1b8051a4a
heisid <weird_alien@protonmail.com>
2018-07-31 09:38:39 +0700

    more on profile controller

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -11,7 +11,15 @@ class Profile extends CI_Controller {
     }
 
     public function index($username) {
-        $user_data = $this->User_model->get_user_detail();
+        $user_detail = $this->User_model->get_user_detail();
+        $user_role = $this->User_model->get_user_role();
+        $posts_list = $this->Post_model->posts_by_author($username);
+
+        $user_data = array(
+                     'user_detail' => $user_detail,
+                     'user_role' => $user_role,
+                     'posts_list' => $posts_list
+        );
 
         $this->load->view('header_view', array('page_title' => 'Profile'));
         $this->load->view('navbar_view');
